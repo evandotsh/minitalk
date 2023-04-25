28bccbf8e6b327b368713a59ec27453e77efb097
Evan <me@evan.sh>
2023-04-25 14:58:25 +0200

    selftest: protect invalid pid

--- sources/client/client.c
+++ sources/client/client.c
@@ -6,7 +6,7 @@
 /*   By: evmorvan <evmorvan@student.42nice.fr>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/04/08 13:59:14 by evmorvan          #+#    #+#             */
-/*   Updated: 2023/04/25 14:44:59 by evmorvan         ###   ########.fr       */
+/*   Updated: 2023/04/25 14:57:42 by evmorvan         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -77,8 +77,10 @@ int	main(int argc, char **argv)
 	int					len;
 
 	if (argc != 3 || ft_strlen(argv[2]) <= 0 || is_only_digits(argv[1]))
-		print_and_exit(ERR "Usage: ./client <pid> <text>");
+		print_and_exit(ERR"Usage: ./client <pid> <text>");
 	server_pid = ft_atoi(argv[1]);
+	if (server_pid <= 0)
+		print_and_exit(ERR"Invalid server pid");
 	sigemptyset(&sg_action.sa_mask);
 	sg_action.sa_flags = SA_SIGINFO;
 	sg_action.sa_sigaction = sig_handler;
--- sources/client/client_bonus.c
+++ sources/client/client_bonus.c
@@ -6,7 +6,7 @@
 /*   By: evmorvan <evmorvan@student.42nice.fr>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/04/08 13:59:14 by evmorvan          #+#    #+#             */
-/*   Updated: 2023/04/25 14:44:53 by evmorvan         ###   ########.fr       */
+/*   Updated: 2023/04/25 14:57:55 by evmorvan         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -81,6 +81,8 @@ int	main(int argc, char **argv)
 	if (argc != 3 || ft_strlen(argv[2]) <= 0 || is_only_digits(argv[1]))
 		print_and_exit(ERR "Usage: ./client <pid> <text>");
 	server_pid = ft_atoi(argv[1]);
+	if (server_pid <= 0)
+		print_and_exit(ERR"Invalid server pid");
 	sigemptyset(&sg_action.sa_mask);
 	sg_action.sa_flags = SA_SIGINFO;
 	sg_action.sa_sigaction = sig_handler;
